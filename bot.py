c227cfb87e01f1436cfbdb5dc7dbd00c35857fa6
FunXD-0 <72953941+FunXD-0@users.noreply.github.com>
2021-09-10 20:46:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,4 +45,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('Insert Discord Token Here')
+client.run('Insert Discord Bot Token Here')
